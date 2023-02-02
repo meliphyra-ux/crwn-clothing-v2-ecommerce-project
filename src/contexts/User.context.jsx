1dad603f382cdc51cf9f9f23fea48d3a07ed954b
meliphyra-ux <xennerit@icloud.com>
2023-02-02 23:43:43 +0200

    Redirects

--- src/contexts/User.context.jsx
+++ src/contexts/User.context.jsx
@@ -10,10 +10,10 @@ export const UserContext = createContext({
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const value = { currentUser, setCurrentUser };
-
+  
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
-      if(user) createUserDocumentFromAuth(user); 
+      if(user) createUserDocumentFromAuth(user);
       setCurrentUser(user)})
     return unsubscribe
   }, [])
